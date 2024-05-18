8801d9ef1e6496db604161c46922d2c874337a64
marcosherna <marcosxdf6@gmail.com>
2024-05-18 10:33:03 -0600

    add router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { LoginComponent } from './components/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/login', pathMatch: 'full' },
+    { path: 'login', component: LoginComponent}, 
+    { path: '**', component: PageNotFoundComponent },
+];
