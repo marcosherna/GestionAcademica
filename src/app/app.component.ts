9b5290551d93000cface26f4342f8d46e0f29e11
marcosherna <marcosxdf6@gmail.com>
2024-05-17 22:34:01 -0600

    add tailwindcss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,21 +3,36 @@ import { RouterOutlet } from '@angular/router';
 import EstudiantesService from './services/estudiantes.service';
 import { CommonModule } from '@angular/common';
 
+import { ButtonModule } from 'primeng/button';
+import { TreeModule } from 'primeng/tree';
+import { TreeNode } from 'primeng/api';
+
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
+  imports: [
+    RouterOutlet, 
+    CommonModule, 
+    TreeModule,
+    ButtonModule],
   templateUrl: './app.component.html', 
+  styleUrl: './style.css'
 })
 export class AppComponent {
   title = 'GestionAcademica';
+
+  files!: TreeNode[];
+
+    constructor() {}
+
+    ngOnInit() { 
+    } 
 
   private readonly estudiantesServices = inject(EstudiantesService);
 // Creando observable
   estudiantes$ = this.estudiantesServices.obtenerEstudiantes();
 
-  constructor() {
-    // this.estudiantesServices.postEstudiante().subscribe((data:any) => { console.log(data) });
-  }
+ 
 
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,20 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 
+
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient(withFetch())
+    provideHttpClient(withFetch()), 
+    importProvidersFrom([
+      BrowserModule,
+      BrowserAnimationsModule])
+    
   ]
 };
